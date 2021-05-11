2a88b09c7e68dfe8b33f7bb3c5ec0f13148ed6e9
Daniil Antsupov <antsupov.official@gmail.com>
2021-05-12 05:47:27 +0700

    dev Huge update, add JWT tokens

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
+require('dotenv').config();
+
 const Gateway = require('micromq/gateway');
+const redis = require('redis');
+
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
+
+//middleware
+const authCheck = require('./src/middlewares/authCheck');
+
+//modules
+const authorization = require('./src/modules/authorization');
 
 const app = new Gateway({
   microservices: ['auth'],
   rabbit: {
-    url: 'amqp://hasty:superhardpassword@rabbitmq:5672',
+    url: process.env.RABBIT_URL,
   },
 });
 
-// создаем два эндпоинта /friends & /status на метод GET
-app.get(['/login'], async (req, res) => {
-  // делегируем запрос в микросервис users
-  await res.delegate('users');
+app.enablePrometheus();
+
+console.log(app);
+
+app.post(['/updateTokens'], async (req, res) => {
+  await authorization.requestNewTokens(req, res, client);
 });
 
-app.listen(8080);
+app.get(['/test'], authCheck(client), async (req, res) => {
+  console.log(req);
+  await res.delegate('auth');
+});
+
+console.log('App listen on port 8045');
+
+app.listen(8045);
